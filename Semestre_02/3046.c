f82e86dd22b1a632d341dc72a5e7b9ef3c58e740
C4ssinelli <101810975+C4ssinelli@users.noreply.github.com>
2024-10-18 07:59:08 -0300

    Create 3046.c

---
+++ Semestre_02/3046.c
@@ -0,0 +1,10 @@
+/*Eu pegeui a formula dada pelo exercicio e a escrevi igualando a uma variavel.*/
+#include <stdio.h>
+int main ()
+{
+	int n, R;
+	scanf ("%d", &n);
+	R = ((n+1)*(n+2))/2;
+	printf("%d\n", R);
+	return 0;
+}
